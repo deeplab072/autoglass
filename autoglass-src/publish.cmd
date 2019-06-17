a6487f542d1d7591dad2f05027f6ce3d9de31cb3
substring072 <51917172+substring072@users.noreply.github.com>
2019-06-17 22:42:16 +0900

    first

---
+++ autoglass-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=autoglass-b5f2-Web-Deploy.pubxml -p:Password=RZqZLiNDma2NaeyGHlHoNQqce1LDTgDuoMu5nEgrER02GP1y4PfDaJb20RvA
+
